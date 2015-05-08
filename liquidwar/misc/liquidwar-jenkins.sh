6c7891346e123addd5e9db21e35897d701dfc2cc
Christian Mauduit <ufoot@ufoot.org>
2015-05-08 14:57:29 +0200

    Using bootstrap script in Jenkins

--- liquidwar/misc/liquidwar-jenkins.sh
+++ liquidwar/misc/liquidwar-jenkins.sh
@@ -28,29 +28,35 @@
 echo "******** $0 $(date) ********"
 if cd liquidwar ; then
     echo "******** $0 $(date) ********"
-    if ./configure --prefix=$WORKSPACE/local ; then
+    if ./bootstrap; then
 	echo "******** $0 $(date) ********"
-	if make ; then
+	if ./configure --prefix=$WORKSPACE/local ; then
 	    echo "******** $0 $(date) ********"
-	    if make install; then
+	    if make ; then
 		echo "******** $0 $(date) ********"
-	        if make dist; then
+		if make install; then
 		    echo "******** $0 $(date) ********"
-	        else
-		    echo "make dist failed"
+	            if make dist; then
+			echo "******** $0 $(date) ********"
+	            else
+			echo "make dist failed"
+			exit 6
+	            fi
+		else
+		    echo "make install failed"
 		    exit 5
-	        fi
+		fi
 	    else
-		echo "make install failed"
+		echo "make failed"
 		exit 4
 	    fi
 	else
-	    echo "make failed"
+	    echo "./configure failed"
 	    exit 3
 	fi
     else
-	echo "./configure failed"
-	exit 2
+	echo "./bootstrap failed"
+	exit 2	   
     fi
 else
     echo "cd failed"
